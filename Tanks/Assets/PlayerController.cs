9e6c8c700b391fb183908bf5bec8017ad1d3f5b8
HarrisonPearl <Hspearl@gmail.com>
2020-02-09 18:50:03 -0600

    got backward movement working

--- Tanks/Assets/PlayerController.cs
+++ Tanks/Assets/PlayerController.cs
@@ -10,21 +10,22 @@ public class PlayerController : MonoBehaviour
     public float rotateSpeed = 120f;
 
     public Rigidbody2D rb;
-    public float moveAngleThresh = 3;
+    public float moveAngleThresh = 35;
     public float turnThresh = 90;
 
     private float angle;
+    private float oppAngle;
     private Quaternion qAngle;
     private Quaternion qAngle180;
     private Quaternion pAngle180;
     private float change;
 
 
-    Vector2 movement;
+    private Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -39,32 +40,49 @@ public class PlayerController : MonoBehaviour
     {
         if (Mathf.Abs(movement.x) > 0.1 || Mathf.Abs(movement.y) > 0.1)
         {
-            
-            angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg + -90;
+
+            angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90;
+            if (angle >= 0){
+              oppAngle = angle - 180;
+            }
+            else{
+              oppAngle = angle + 180;
+            }
+            //Debug.Log(oppAngle);
+
             qAngle = Quaternion.AngleAxis(angle, Vector3.forward);
-            qAngle180 = qAngle;
-            qAngle180.y = -qAngle180.y;
-            qAngle180.z = -qAngle180.z;
-            if (Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle)) > turnThresh)
-            {
-                rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, qAngle180, rotateSpeed * Time.deltaTime);
+            qAngle180 = Quaternion.AngleAxis(oppAngle, Vector3.forward);
+
+            //Debug.Log(Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle)));
+            if (Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle)) <= turnThresh){
+              rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, qAngle, rotateSpeed * Time.deltaTime);
+
+              if (Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle)) <= moveAngleThresh){
+                rb.MovePosition(rb.position + (Vector2)rb.transform.up * moveSpeed * Time.fixedDeltaTime);
+              }
             }
-            else
-            {
-                rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, qAngle, rotateSpeed * Time.deltaTime);
+            else{
+              rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, qAngle180, rotateSpeed * Time.deltaTime);
+
+              if (Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle180)) <= moveAngleThresh){
+                rb.MovePosition(rb.position + -1* (Vector2)rb.transform.up * moveSpeed * Time.fixedDeltaTime);
+              }
             }
-            
+
+
 
         }
-        pAngle180 = rb.transform.rotation;
-        pAngle180.y = -pAngle180.y;
-        pAngle180.z = -pAngle180.z;
-        if (Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle)) <= moveAngleThresh || Mathf.Abs(Quaternion.Angle(pAngle180, qAngle)) <= moveAngleThresh)
-        {
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        }
+
+
+
+        // pAngle180 = rb.transform.rotation;
+        // pAngle180.y = -pAngle180.y;
+        // pAngle180.z = -pAngle180.z;
+        // if (Mathf.Abs(Quaternion.Angle(rb.transform.rotation, qAngle)) <= moveAngleThresh || Mathf.Abs(Quaternion.Angle(pAngle180, qAngle)) <= moveAngleThresh)
+        // {
+        //     rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // }
     }
 
 
 }
-
