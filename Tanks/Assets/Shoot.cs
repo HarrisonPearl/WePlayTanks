39af08ab40a19fcab73be43d0235c492d2515dda
HarrisonPearl <Hspearl@gmail.com>
2020-02-13 20:09:34 -0600

    added shooting
    particle system is acting wonky but shooting works

---
+++ Tanks/Assets/Shoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float fireRate;
+    public float bulletSpeed;
+    public float offSet;
+    public GameObject bullet;
+
+
+    private float nextShot;
+    private float shoot;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       shoot = Input.GetAxis("RightTrigger");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(shoot);
+        if(shoot > 0.9 && Time.time > nextShot)
+        {
+            nextShot = Time.time + fireRate;
+            GetComponent<ParticleSystem>().Play();
+            var bulletClone = Instantiate(bullet, transform.position + transform.up * offSet, transform.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed * 10);
+            
+        }
+
+    }
+}
