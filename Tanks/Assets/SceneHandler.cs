9944a57d08d4805240f28a1a0684b93e694a9778
VictorAung <VictorAung2021@u.northwestern.edu>
2020-02-26 17:45:39 -0600

    level management

---
+++ Tanks/Assets/SceneHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class SceneHandler : MonoBehaviour
+{
+    private GameObject[] playerArray;
+    private int nextSceneIndex;
+    private float currTime;
+    private float victoryTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        victoryTime = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(victoryTime);
+        currTime = Time.fixedTime;
+        playerArray = GameObject.FindGameObjectsWithTag("Player"); 
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+
+        if (playerArray.Length <= 1)
+        {
+
+            if (victoryTime == 0)
+            {
+                victoryTime = currTime;
+            }
+
+            else if (currTime - victoryTime >= 3)
+            {
+                if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+                {
+                    SceneManager.LoadScene(nextSceneIndex);
+                }
+                else
+                {
+                    SceneManager.LoadScene(0);
+                }
+            }
+        }
+       
+        
+    }
+}
