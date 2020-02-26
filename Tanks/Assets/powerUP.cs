f3daf581532c2b59923f507b8f993753fb7a9c57
HarrisonPearl <Hspearl@gmail.com>
2020-02-26 17:32:35 -0600

    added weapon powerups
    new level including weapon power up added

---
+++ Tanks/Assets/powerUP.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUP : MonoBehaviour
+{
+    public string type;
+    public GameObject newGun;
+
+    private Transform gun = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<PlayerController>())
+        {
+            //Debug.Log("player hit");
+            gun = collision.transform.parent.Find("Tower").Find("Gun");
+            var nGun = Instantiate(newGun, gun.parent);
+            nGun.transform.position = gun.position;
+            nGun.transform.rotation = gun.rotation;
+            Destroy(gun.gameObject);
+
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Tanks/Assets/shoot3.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot3 : MonoBehaviour
+{
+    public float fireRate;
+    public float bulletSpeed;
+    public float offSet;
+    public GameObject bullet;
+    public string PlayerTrigger;
+
+    private float nextShot;
+    private float shoot;
+    private float sprayAngle = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot = Input.GetAxis(PlayerTrigger);
+    }
+
+    void FixedUpdate()
+    {
+
+
+        if (shoot > 0.9 && Time.time > nextShot)
+        {
+            nextShot = Time.time + fireRate;
+            GetComponent<ParticleSystem>().Play();
+            var bulletClone0 = Instantiate(bullet, transform.position + transform.up * offSet, transform.rotation);
+            var bulletClone1 = Instantiate(bullet, transform.position + transform.up * offSet, transform.rotation*Quaternion.Euler(0, 0, -sprayAngle));
+            var bulletClone2 = Instantiate(bullet, transform.position + transform.up * offSet, transform.rotation*Quaternion.Euler(0, 0, sprayAngle));
+            bulletClone0.GetComponent<Rigidbody2D>().AddForce(bulletClone0.transform.up * bulletSpeed * 10);
+            bulletClone1.GetComponent<Rigidbody2D>().AddForce(bulletClone1.transform.up * bulletSpeed * 10);
+            bulletClone2.GetComponent<Rigidbody2D>().AddForce(bulletClone2.transform.up * bulletSpeed * 10);
+
+        }
+
+    }
+}
