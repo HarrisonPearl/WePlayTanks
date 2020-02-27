a3718f265d463b34b6cb344e5c64188ced796b3f
HarrisonPearl <Hspearl@gmail.com>
2020-02-26 18:54:01 -0600

    destructible walls

---
+++ Tanks/Assets/destructable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructable : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Missile")){
+            transform.localScale -= new Vector3(0.01f, 0.01f, 0);
+            health -= 1;
+            if (health < 1)
+            {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
