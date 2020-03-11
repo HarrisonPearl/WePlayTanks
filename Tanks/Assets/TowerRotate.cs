8cccbc72cfbb83cad53c09103fd28c11967fb8d3
HarrisonPearl <Hspearl@gmail.com>
2020-03-11 14:18:45 -0500

    tower rotate tuning
    towe rotate speed now modulated by distance of joystick movement

--- Tanks/Assets/TowerRotate.cs
+++ Tanks/Assets/TowerRotate.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class TowerRotate : MonoBehaviour
 {
     private Vector2 movement;
-    public float rotateSpeed = 250f;
+
     private float angle;
     private Quaternion qAngle;
     public Rigidbody2D tankRb;
 
     public string PlayerRotateX;
     public string PlayerRotateY;
+
+    private float modSpeed;
+    private float rotateSpeed = 800f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,11 +32,13 @@ public class TowerRotate : MonoBehaviour
     void FixedUpdate()
     {
         transform.position = tankRb.position;
-        if (Mathf.Abs(movement.x) > 0.05 || Mathf.Abs(movement.y) > 0.05)
+        if (Mathf.Abs(movement.x) > 0.01 || Mathf.Abs(movement.y) > 0.01)
         {
+            modSpeed = rotateSpeed * (Mathf.Abs(movement.x) + Mathf.Abs(movement.y)) / 2;
+
             angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90;
             qAngle = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, qAngle, rotateSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, qAngle, modSpeed * Time.deltaTime);
 
         }
     }
